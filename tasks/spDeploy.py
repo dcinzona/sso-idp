c1672873b44f95f6cd3da384f9f60f30ec90db95
Gustavo Tandeciarz <gmt@tandeciarz.com>
2024-06-28 23:28:49 -0400

    adding cert flows

--- tasks/spDeploy.py
+++ tasks/spDeploy.py
@@ -2,6 +2,10 @@ import os
 import copy
 
 from tasks.idpDeploy import Deploy
+from cumulusci.salesforce_api.metadata import ApiRetrieveUnpackaged
+from cumulusci.tasks.salesforce import BaseRetrieveMetadata
+
+
 from cumulusci.utils import temporary_dir
 from cumulusci.salesforce_api.utils import get_simple_salesforce_connection
 deploy_options = copy.deepcopy(Deploy.task_options)
@@ -28,42 +32,7 @@ METADATA_XML = """<?xml version="1.0" encoding="UTF-8"?>
     <useConfigRequestMethod>true</useConfigRequestMethod>
     <useSameDigestAlgoForSigning>true</useSameDigestAlgoForSigning>
     <userProvisioning>true</userProvisioning>
-    <validationCert>MIIGdTCCBF2gAwIBAgIOAZBfsZGGAAAAAEPd9SMwDQYJKoZIhvcNAQELBQAwfzEX
-MBUGA1UEAwwOU2VsZlNpZ25lZENlcnQxGDAWBgNVBAsMDzAwRDNSMDAwMDAwQWh0
-UDEXMBUGA1UECgwOU2FsZXNmb3JjZS5jb20xFjAUBgNVBAcMDVNhbiBGcmFuY2lz
-Y28xCzAJBgNVBAgMAkNBMQwwCgYDVQQGEwNVU0EwHhcNMjQwNjI4MTYzMzExWhcN
-MjYwNjI4MTIwMDAwWjB/MRcwFQYDVQQDDA5TZWxmU2lnbmVkQ2VydDEYMBYGA1UE
-CwwPMDBEM1IwMDAwMDBBaHRQMRcwFQYDVQQKDA5TYWxlc2ZvcmNlLmNvbTEWMBQG
-A1UEBwwNU2FuIEZyYW5jaXNjbzELMAkGA1UECAwCQ0ExDDAKBgNVBAYTA1VTQTCC
-AiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK8NDpdbJWeTOYVTgtrIq0x9
-tuj+XFgCoIMwRRnYMdI5lzB3yxZlvqmNFuDMy0f2NXJuJnbt4rQjrdaFtS0/CSst
-FUl4RLynZXqXDh/UghbKS9bWloH9AGPrQWN9ca+OTIbbH3Ozlcts3cwvfAwQWqOM
-RYrUSjW+rgwx+OoBgpXYzcPPZ8qgXWFi6I6+kqYG2GubY+N7TCfeFhtPj32a+vcE
-2H74HFcWaejPDL6tUsu51ztXmpV75d2VEqJYi4hijw+NnB6yhZuI1anl1cFzVTHL
-E1PHZrYpwiBhi0H79BAEiOKhbqqAzTf+AtlpgreOPUYLr1AcIFJ0SZq1KRWBnJ+9
-xj3v4BbbrcmQpbgWSc/CYxi9NlPCGdaTVV51rj0QvTvua5AnPwV7hmgPmQuRQqyd
-+KOZ4ldNqSCVrPRdfqLEr1lgzSi+vfAVqQ5N2gylj7ot0Bd9ANvbGDrP+Qv9/SaX
-d6LiEAnQ1kFNhKJBXdr9QcSiYQBZbO6IK2grBKEZeEvJ5Roo7bTEvud1V4G6neae
-Hl+4wAm46WWpe/FjN2h2MBpCkNd+NLpkjyc9EBQgNP6Xa85A7njCE2hJe1CQkQsu
-oV9dHyhHUjyKbLyQeiomIehezb3h+kN8QjfxRVTQHKFIaMe0oW5Zaahkd1eF955Y
-DF7ibTXIwXJglWR1M2JbAgMBAAGjge4wgeswHQYDVR0OBBYEFG4gR9fIG6I0cU3S
-gkmV3ZFLsGcsMA8GA1UdEwEB/wQFMAMBAf8wgbgGA1UdIwSBsDCBrYAUbiBH18gb
-ojRxTdKCSZXdkUuwZyyhgYSkgYEwfzEXMBUGA1UEAwwOU2VsZlNpZ25lZENlcnQx
-GDAWBgNVBAsMDzAwRDNSMDAwMDAwQWh0UDEXMBUGA1UECgwOU2FsZXNmb3JjZS5j
-b20xFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xCzAJBgNVBAgMAkNBMQwwCgYDVQQG
-EwNVU0GCDgGQX7GRhgAAAABD3fUjMA0GCSqGSIb3DQEBCwUAA4ICAQAf/6WBbWgx
-Q89to6UOFlXBXCYIqOs5rgRpybCh7mjbacB4MrgJTf07iaI/Q8qYH7+8fBSc8lZ8
-pPWeZX/xjhAxpmTlEGW9BasGOVFh/IEZj+vfSXdYtpMH2cQLO7sbwYQ1dCM/ltQi
-cjrzKnZ5jGcffen9Y/1nwtA2G4z6sdA5T2Xodi0gTO6c7+8SKHa1O2//yJWrF+Wg
-2W/FtiXRuLKaGYYZoYpuIbStO5y4SODqx+GpddTUCZDDS4v2nk7GDCCuhACqzbNr
-rQSAavRzxyK6G5u9pfbwK7XZzmOTfLI/F4WqAwTf3WJwztIoSVH326TULxT3+41z
-9dyTQez2ycNU1mUJB0IMzZG8WUJKmxOL4N2UXl6luFXDjvAXaRypIiM6wtfEozo3
-m44G4p/grncwE8+eHHT6ahap+gRuannUzATjLQSbzO8p9PScln8/RugatVuCPfs7
-0l+DKTBPyCmGKmHKsIQPaulm0A9EkjIII2/yAjnBmkzUwWhqEWkHnXlnz6VOZ21y
-v6Q80UrArKqwO7kVF9OwRc6nk09qvDsU4zNY5xPuozAVxEr6UyJ+S/PHfHvMBnfV
-nnmRGZsWFQChTpOiLDsl63PEOwWcmm0a7Hk4d7Rvw8+K1u7/a1hpS5vS3ABw6N8T
-ozarc1gNimDP9sFmsq4bHvcm0m/uZXIHFA==
-    </validationCert>
+    <validationCert>{idp_cert}</validationCert>
 </SamlSsoConfig>"""
 
 PACKAGE_XML = """<?xml version="1.0" encoding="UTF-8"?>
@@ -106,7 +75,7 @@ class deploySSO(Deploy):
         self.user_id = self.org_config.user_id[:15]
         super()._init_options(kwargs)
 
-    def _get_cert_id(self, certName='SelfSignedCert') -> str | None:
+    def _get_cert_id(self, certName='SelfSignedCert') -> str:
         if self.cert_id:
             return self.cert_id
         # get the cert id from the org
@@ -114,7 +83,7 @@ class deploySSO(Deploy):
         self.cert_id = self._get_record_id_from_query(query, self.tooling)
         return self.cert_id
 
-    def _get_jit_class_id(self, name='JitHandler') -> str | None:
+    def _get_jit_class_id(self, name='JitHandler') -> str:
         if self.jit_hanlder:
             return self.jit_hanlder
         # get the cert id from the org
@@ -136,7 +105,8 @@ class deploySSO(Deploy):
             idp_org_url=self.idp_url,
             sp_org_url=self.sp_url,
             cert_id=self._get_cert_id(),
-            jit_apex_class=self._get_jit_class_id()
+            jit_apex_class=self._get_jit_class_id(),
+            idp_cert=self.cert,
         )
         with open(
             os.path.join(metapath, 'IdP' + ".samlssoconfig"),
@@ -146,11 +116,69 @@ class deploySSO(Deploy):
             f.write(metaxml)
         with open("package.xml", "w") as f:
             f.write(PACKAGE_XML)
+    
+    def _get_cert_from_idp(self):
+        retrieve_cert = RetrieveCert(project_config=self.project_config,
+                                     task_config=self.task_config,
+                                     org_config=self.idpOrgConfig,
+                                     logger=self.logger)
+        retrieve_cert()
+        self.cert = retrieve_cert.get_cert()
 
     def _run_task(self):
+        self._get_cert_from_idp()
         with temporary_dir() as tempdir:
+            if not self.cert:
+                self.logger.error("No cert found")
+                return
             self.tempdir = tempdir
             self._build_package()
             self.options["path"] = tempdir
             self.task_options["path"] = tempdir
             super()._run_task()
+
+
+retrieve_unpackaged_options = BaseRetrieveMetadata.task_options.copy()
+retrieve_unpackaged_options["path"][
+    "required"
+] = False
+
+
+class RetrieveCert(BaseRetrieveMetadata):
+    api_class = ApiRetrieveUnpackaged
+    api_version = "59.0"
+    task_options = retrieve_unpackaged_options
+
+    def _init_options(self, kwargs):
+        kwargs["package_xml"] = 'manifest/certs.xml'
+        super(RetrieveCert, self)._init_options(kwargs)
+        if "package_xml" in self.options:
+            with open(self.options["package_xml"], "r") as f:
+                self.options["package_xml_content"] = f.read()
+
+    def _run_task(self):
+        with temporary_dir() as tempdir:
+            self.options["path"] = tempdir
+            super()._run_task()
+            # read directory content
+            self.logger.info(f"Tempdir: {tempdir}")
+            self.logger.info(f"Contents: {os.listdir(tempdir)}")
+            # check if certs directory exists
+            if not os.path.exists(os.path.join(tempdir, 'certs')):
+                self.logger.error(f"Cert directory not found in {tempdir}")
+                return
+            # read the cert
+            with open(os.path.join(tempdir, 'certs', 'SelfSignedCert.crt'), 'r') as f:
+                self.cert = f.read()
+                self.cert = self.cert.replace('-----BEGIN CERTIFICATE-----\n', '')
+                self.cert = self.cert.replace('-----END CERTIFICATE-----\n', '')
+                # self.cert = self.cert.replace('\n', '')
+            self.logger.info(f"Cert: {self.cert}")
+    
+    def get_cert(self):
+        return self.cert
+
+    def _get_api(self):
+        return self.api_class(
+            self, self.options["package_xml_content"], self.options.get("api_version")
+        )
