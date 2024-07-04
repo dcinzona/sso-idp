1b5f5197df0ceca26233b27583ba3ee0857bff7a
Gustavo Tandeciarz <gustavo.tandeciarz@salesforce.com>
2024-07-04 00:44:48 -0400

    alpha

--- tasks/installPackage.py
+++ tasks/installPackage.py
@@ -1,18 +1,65 @@
 import copy
-from logging import root
 from math import e
-from struct import pack
+from re import sub
 from cumulusci.tasks.salesforce.install_package_version import InstallPackageVersion
-from cumulusci.core.config import TaskConfig
-from cumulusci.core.config.org_config import OrgConfig
-from cumulusci.tasks.sfdx import SFDXBaseTask, SFDXOrgTask
+from cumulusci.tasks.sfdx import SFDXOrgTask
 from cumulusci.tasks.salesforce import BaseSalesforceApiTask
+from cumulusci.salesforce_api.utils import get_simple_salesforce_connection
+from cumulusci.core.config.util import get_devhub_config
+from cumulusci.core.dependencies.utils import TaskContext
+from cumulusci.core.utils import process_bool_arg, process_list_arg
+from cumulusci.core.config import TaskConfig
+import pprint as pp
+import json
 
 install_options = copy.deepcopy(InstallPackageVersion.task_options)
 install_options["name"]["required"] = True
 install_options["namespace"]["required"] = False
 
+package_query_fields = [
+    "Id",
+    "Package2.Name",
+    "Name",
+    "SubscriberPackageVersionId",
+    "IsReleased",
+    "ReleaseVersion",
+    "MajorVersion",
+    "MinorVersion",
+    "PatchVersion",
+    "BuildNumber",
+    ]
 # cci task run dx --command "package install --package 'identity-common'" --org idp
+
+
+class PackageVersion(object):
+    def __init__(self, package2VersionRecord):
+        rec = dict(package2VersionRecord)
+        for key in package_query_fields:
+            setattr(self, key, rec[key]) if key in rec.keys() else None
+        self.version = f"{self.MajorVersion}.{self.MinorVersion}.{self.PatchVersion}"
+        if self.BuildNumber is not None:
+            self.version += f".{self.BuildNumber}"
+    
+    def __str__(self):
+        return json.dumps(self.__dict__, indent=4)
+    
+    def __repr__(self):
+        return json.dumps(self.__dict__, indent=4)
+
+class PackageDependency(object):
+    def __init__(self, deps):
+        if "ids" in deps:
+            setattr(self, "ids", deps["ids"])
+            sub_ids = [x['subscriberPackageVersionId'] for x in deps["ids"]]
+            setattr(self, "ids_csv", ",".join(sub_ids))
+                
+        
+    
+    def __str__(self):
+        return json.dumps(self.__dict__, indent=4)
+    
+    def __repr__(self):
+        return json.dumps(self.__dict__, indent=4)
 
 class InstallPackageDeps(BaseSalesforceApiTask):
     task_options = {
@@ -24,12 +71,19 @@ class InstallPackageDeps(BaseSalesforceApiTask):
             "description": "The version of the package to install",
             "required": False,
         },
+        "query_only": {
+            "description": "The version of the package to install",
+            "required": False,
+            "default": False
+        },
     }
     root_package = None
     packages = None
 
     def _init_options(self, kwargs):
-        super()._init_options(kwargs)
+        super(InstallPackageDeps, self)._init_options(kwargs)
+        self.logger.info(f"Options: {self.options}")
+        self.query_only = process_bool_arg(self.options.get("query_only", False))
         self.options["version"] = "latest"
         packageName = self.options['name']
         sfdxConfig = self.project_config.sfdx_project_config
@@ -102,27 +156,171 @@ class InstallPackageDeps(BaseSalesforceApiTask):
             package_ids.append(package_id)
         return package_ids
 
-    def _create_task_for_deps(self, dependencies):
-        for dependency in dependencies:
+    def _create_task_for_deps(self, list_of_versionIds):
+        for dependency in list_of_versionIds:
             task_options = {
-                "command": "package install",
-                "extras": "--package " + dependency["subscriberPackageId"],
+                "command": f"package install --package {dependency}"
             }
-            task = self.create_task(
-                SFDXOrgTask,
-                task_options,
-                project_config=self.project_config,
-                org_config=self.org_config
-            )
-            task()
 
+            task = SFDXOrgTask(self.project_config,
+                               TaskConfig({"options": task_options}),
+                               self.org_config)
+            task()
+    
+    def _query_dev_hub(self, package_name, version=None):
+        query = f"SELECT {','.join(package_query_fields)} FROM Package2Version WHERE Package2.Name = '{package_name}'"
+        self.logger.info(f"\nQuerying Dev Hub for package: {package_name}")
+        self.logger.info(f"Query: {query}\n")
+        result = self.tooling.query(query)
+        arr = [PackageVersion(x) for x in result["records"]]
+        sorted_arr = sorted(arr, key=lambda x: x.version)
+        self.logger.info(f"Found Package Versions:\n{pp.pformat(sorted_arr)}")
+        if version is not None:
+            self.logger.info(f"Filtering for version: {version}")
+            return [x for x in sorted_arr if x.version == version]
+        return sorted_arr
+    
+
+    
+    def _query_dependencies(self, package_version_id):
+        self.logger.info(f"Querying dependencies for package version: {package_version_id}")
+        query = f"SELECT Dependencies FROM SubscriberPackageVersion WHERE Id = '{package_version_id}'"
+        self.logger.info(f"Query: {query}\n")
+        result = self.tooling.query(query)
+        results = [PackageDependency(r['Dependencies']) for r in result["records"]]
+        return results
+        # return [r['Ids'] for r in result["records"]]
+
+        
+
+    def _get_package_version_id(self, package_name, version="latest"):
+        if package_name.startswith('04t'):
+            return package_name
+        if package_name in self.aliases:
+            for alias in self.aliases:
+                if self.aliases[alias].startswith('04t') and alias == package_name:
+                    return self.aliases[alias]
+        
+        return None
+
+    def _init_task(self):
+        self.tooling = get_simple_salesforce_connection(
+            self.project_config,
+            get_devhub_config(self.project_config),
+            api_version=self.project_config.project__package__api_version,
+            base_url="tooling",
+        )
+        self.context = TaskContext(self.org_config, self.project_config, self.logger)
+
+    def _get_package_from_packages(self, dependency):
+        package_name = dependency["package"] if "package" in dependency else None
+        if package_name is None:
+            return None
+        for package in self.packages:
+            if package["package"] == package_name:
+                return package
+        return None
+    
+    def _query_only(self):
+        versions = self._query_dev_hub(self.options["name"])        
+        self.logger.info(f"Found Package Versions:\n{pp.pformat(versions)}")
+        return versions
+    
+    def _get_deps_from_sfdx_project_package(self, package):
+        deps = []
+        self.logger.info(f"Getting dependencies for package: {package['package']}")
+        if "dependencies" in package:
+            self.logger.info(f"Found dependencies in sfdx-project.json {package['dependencies']}")
+            for dep in package["dependencies"]:
+                if "subscriberPackageVersionId" in dep:
+                    deps.append(
+                        PackageDependency(
+                            {"ids": [
+                                {"subscriberPackageVersionId": dep["subscriberPackageVersionId"]}
+                                ]
+                            })
+                        )
+                elif "package" in dep:
+                    for found in self._query_dev_hub(dep["package"]):
+                        deps.append(
+                            PackageDependency(
+                                {"ids": [
+                                    {"subscriberPackageVersionId": found.SubscriberPackageVersionId}
+                                    ]
+                                })
+                            )
+                        deps += self._query_dependencies(found.SubscriberPackageVersionId)
+                    return list(set(deps))
+        else:
+            self.logger.info("No dependencies found in sfdx-project for package")
+        return deps
+
+    def create_task(task_class, options=None, project_config=None, org_config=None):
+        if options is None:
+            options = {}
+        task_config = TaskConfig({"options": options})
+        return task_class(project_config, task_config, org_config)
+            
     def _run_task(self):
-        self.logger.info(f"Installing package: {self.options["name"]}")
-        if self.root_package is None:
-            self.logger.info("Package not found in sfdx-project.json")
+
+        if self.query_only:
+            versions = self._query_only()
+            deps = []
+            if len(versions) == 1:
+                deps = self._query_dependencies(versions[0].SubscriberPackageVersionId)
+                self.logger.info(f"Found Dependencies:\n{pp.pformat(deps)}")
+            elif len(versions) > 1:
+                self.logger.info("Multiple versions found, please specify a version")
+            else:
+                self.logger.info("No versions found for package in dev hub")
+                # get deps from sfdx-project.json
+                package = self._get_package_from_packages_by_name(self.options["name"], self.packages)
+                if package is not None:
+                    deps = self._get_deps_from_sfdx_project_package(package)
+            
+            list_of_dep_ids = []
+            for dep in deps:
+                for ids in dep.ids:
+                    list_of_dep_ids.append(ids['subscriberPackageVersionId'])
+
+            uniques = list(set(list_of_dep_ids))
+            self._create_task_for_deps(uniques)
+            
             return
-        root_package_name = self.root_package["package"]
-        dependencies = self._get_dependencies_for_package(root_package_name)
+
+        self.logger.info(f"Getting dependencies for: {self.options["name"]}")
+        if self.root_package is None:
+            self.logger.info("Main Package Version not found in sfdx-project.json")
+            return
+        
+        packageName = self.root_package["package"]
+        rootSubscriberId = self._get_subscriber_package_id(packageName)
+        dependencies = []
+        if rootSubscriberId is None:
+            self.logger.info(f"Subscriber Package Id not found for package: {packageName}")
+            self.logger.info("Assuming package dependencies are in sfdx-project.json")
+            for dep in self.root_package["dependencies"]:
+                self.logger.info(f"Found root package dependency: {dep}")
+                pack = self._get_package_from_packages(dep)
+                if pack is not None:
+                    self.logger.info(f"Found package: {pack}")
+                    dep_alias = self._get_subscriber_package_id(pack["package"])
+                    if dep_alias is not None:
+                        self.logger.info(f"Found Subscriber Package Id: {dep_alias}")
+                        dependencies.union(self._query_dependencies(dep_alias))
+                        for dependency in dependencies:
+                            self.logger.info(f"Found dependency: {dependency['Dependencies']}")
+                    else:
+                        self.logger.info(f"Subscriber Package Id not found for package: {pack['package']}")
+                        pp.pprint(self._query_dev_hub(pack["package"]))
+                else:
+                    self.logger.info(f"Package not found in sfdx-project.json")
+        else:
+            self.logger.info(f"Found Subscriber Package Id: {rootSubscriberId}")
+            dependencies = self._query_dependencies(rootSubscriberId)
+
         for dependency in dependencies:
-            self.logger.info(f"Found dependency: {dependency}")
+            self.logger.info(f"Found dependency: {dependency['Dependencies']}")
+        return
+        
         
